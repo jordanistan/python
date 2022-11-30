f37de5e92abde872661998e8798765843f86f7da
jordanistan <jordan.robison@gmail.com>
2022-11-29 23:15:20 -0600

    add madlib

---
+++ miscellaneous/madlib.py
@@ -0,0 +1,26 @@
+# Sting Concatenation ( aka how to put strings together)
+title = "The Ultimate Battle"
+adjective = input("Enter an adjective( “beautiful,” “smooth,” or “heavy” ): ")
+describelook = input("Enter a verb( ugly, hot, or cute ): ")
+verb = input("Enter a verb ( swim, realize, Run, Walk,): ")
+noun = input("Enter a noun(a person, thing, concept, or place) ")
+pronoun = input("Enter a pronoun (he, she, it, they, we, you, or I): ")
+relashionship = input("Enter a relashionship (friend, enemy, lover, or family member): ")
+action = input("Enter an action (fight, run, hide, or cry): ")
+bodypart = input("Enter a body part (head, arm, leg, or eye): ")
+idoit = input("Enter a verb (idoit, asshole, dickhead, potato,): ")
+weapon = input("Enter a weapon: ")
+sound = input("Enter a sound: ")
+
+print(title)
+ninja=f"Dark smoke fills the scene and pump up music slowly gets louder. \
+The audience sees a ninja and his {describelook} {relashionship} {action} at a super expensive restaurant \
+The {relashionship} is so hot that steam is coming out of her {bodypart} \
+Some old {idoit} is sitting by the couple. The {idoit} is giving the {relashionship} the eye and popping like 16 boners. \
+But the ninja sees the boners and the music really pumps up. The audience knows this guy is dead meat for sure. \
+But out of nowhere, the old {idoit} pulls off his jacket to show that he is a pirate with {weapon} and everything. \
+The ninja is like yeah right who cares and then pops the biggest boner ever, bigger than the biggest blackest boner alive. \
+The ninja's boner smashes the entire restaurant.  Every single one of the pirate's boners explodes while making a {sound} sound. \
+The ninja looks back at his {relashionship}.  She smiles and they pork."
+print(ninja)
+# -While writing this script, I head-butt my dog so hard that we both screamed." 
